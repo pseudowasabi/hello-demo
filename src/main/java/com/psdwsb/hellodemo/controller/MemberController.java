e6d2b8818c7f8b36e7d19fb943d49540e7aeea9b
pseudowasabi <y0s30b.k@gmail.com>
2021-10-02 19:51:52 +0900

    [MODIFY] Setter injection (SI) example

--- src/main/java/com/psdwsb/hellodemo/controller/MemberController.java
+++ src/main/java/com/psdwsb/hellodemo/controller/MemberController.java
@@ -7,8 +7,20 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class MemberController {
 
-    @Autowired private MemberService memberService;
+    // ** 3 different types of DI (Dependency injection)
 
+    // 1. Field injection
+    /*@Autowired private MemberService memberService;*/
+
+    // 2. Setter injection
+    private MemberService memberService;
+
+    @Autowired
+    public void setMemberService(MemberService memberService) {
+        this.memberService = memberService;
+    }
+
+    // 3. Constructor injection - 가장 권장하는 방법
     /*@Autowired
     public MemberController(MemberService memberService) {
         *//* @AutoWired: Spring container에 있는 MemberService와 연결시켜 줌 *//*
