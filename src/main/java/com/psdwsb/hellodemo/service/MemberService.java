bc4cc0d7d4acad5138248e6b9f4809f65b813cc8
pseudowasabi <y0s30b.k@gmail.com>
2021-10-02 19:44:20 +0900

    [ADD] SpringConfig for registering spring bean manually

--- src/main/java/com/psdwsb/hellodemo/service/MemberService.java
+++ src/main/java/com/psdwsb/hellodemo/service/MemberService.java
@@ -9,13 +9,11 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
 
-@Service
 public class MemberService {
     /* Service는 비즈니스 로직에 관련한 것, Repository는 데이터를 다루는 입장에서 보기 !
      * 유사한 내용이 들어오게 되더라도, 이렇게 나누어야 나중에 기획에서 요구사항이 변경되어도 로직 변경이 용이해짐 */
     private final MemberRepository memberRepository;
 
-    @Autowired
     public MemberService(MemberRepository memberRepository) {
         // MemberService class 입장에서 보면, MemberRepository가 외부에서 주입된다.
         // = Dependency Injection (DI)
